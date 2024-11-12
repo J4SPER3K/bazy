edeb343e124edbd2d5d2ae2924f195c7b729f785
J4SPER3K <poswiatlolxd@gmail.com>
2024-11-12 13:53:49 +0100

    nauka.sql

--- nauka.sql
+++ nauka.sql
@@ -21,3 +21,17 @@ INSERT INTO walizka (id_walizki, pojemnosc, kolor, id_wlasciciela) VALUES (1, 25
 SELECT * FROM izba;
 
 INSERT INTO izba (adres_budynku, nazwa_izby, metraz, kolor, wlasciciel) VALUES ('13/21 Lesna Dolina','spizarnia','40','biszkoptowy',2);
+
+CREATE TABLE przetwory (
+    id_przetworu INT PRIMARY KEY AUTO_INCREMENT,
+    rok_produkcji YEAR DEFAULT 1654,
+    id_wykonawcy INT,
+    zawartosc VARCHAR(100),
+    dodatek VARCHAR(50) DEFAULT 'papryczka chilli',
+    id_konsumenta INT,
+    FOREIGN KEY (id_wykonawcy) REFERENCES postac(id_postaci),
+    FOREIGN KEY (id_konsumenta) REFERENCES postac(id_postaci)
+);
+
+INSERT INTO przetwory (rok_produkcji, id_wykonawcy, zawartosc, dodatek, id_konsumenta)
+VALUES (2023, 1, 'bigos', DEFAULT, 3);
